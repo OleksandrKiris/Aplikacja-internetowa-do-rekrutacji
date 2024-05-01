7387ff8f94a37d86ffc7a8b2914d5beb28e3c45d
Oleksandr <oleksandr.kiris@icloud.com>
2024-05-01 15:43:44 +0200

    Dodano  modele do panelu administratora

--- communications/admin.py
+++ communications/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'sent_at', 'is_read')
+    search_fields = ('sender__username', 'receiver__username', 'content')
+    list_filter = ('is_read', 'sent_at')
+
+
+admin.site.register(Message, MessageAdmin)
